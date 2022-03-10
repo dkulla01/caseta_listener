7ec6fc738a1768925368205415a0e8213b6135f5
Daniel Kulla <dkulla129@gmail.com>
2022-03-10 14:51:59 -0500

    deserialize hue scene configurations
    this takes the smart_light_finder changes made in this PR:
    https://github.com/dkulla01/smart_light_finder/pull/3

--- src/caseta/scene.rs
+++ src/caseta/scene.rs
@@ -19,30 +19,9 @@ struct Scene {
 #[derive(Deserialize, Debug)]
 #[serde(tag = "type", rename_all = "snake_case")]
 enum Device {
-    HueWhiteAndColorAmbiance {id: Uuid, name: String, on: bool, color: Option<ColorSetting>},
-    NanoleafLightPanels {name: String, on: bool, color: Option<ColorSetting>},
+    HueScene {id: Uuid, name: String},
+    NanoleafLightPanels {name: String, on: bool, effect: String},
     WemoOutlet {name: String, on: bool}
-}
-
-#[derive(Deserialize, Debug)]
-enum ColorSetting {
-    #[serde(rename(deserialize = "xy"))]
-    XYColor(XYColorCoordinates),
-
-    #[serde(rename(deserialize = "effect"))]
-    EffectColorSetting(String)
-}
-
-#[derive(Deserialize, Debug)]
-struct XYColorCoordinates {
-    x: f32,
-    y: f32
-}
-
-impl XYColorCoordinates {
-    fn new(x: f32, y: f32) -> XYColorCoordinates {
-        XYColorCoordinates {x, y}
-    }
 }
 
 #[cfg(test)]
@@ -57,22 +36,16 @@ mod tests {
             remotes: [2, 3]
             scenes:
             - devices:
-              - color:
-                  xy:
-                    x: 0.4575
-                    y: 0.4099
-                id: a3011bb2-dd50-4fd9-b143-7ea03f367088
-                name: Ceiling
-                type: hue_white_and_color_ambiance
-                on: true
+              - id: a3011bb2-dd50-4fd9-b143-7ea03f367088
+                name: warm_reading_light_scene_0
+                type: hue_scene
               - name: Fireplace
                 'on': true
                 type: wemo_outlet
               - name: "Office Shapes"
                 internal_name: LightPanels 01:23:AF
                 'on': true
-                color:
-                  effect: "cozy red"
+                effect: "cozy red"
                 type: nanoleaf_light_panels
               name: white_warmth
             "#;
@@ -83,7 +56,7 @@ mod tests {
         assert_that(&room.scenes[0].name).is_equal_to(String::from("white_warmth"));
         assert_that(&room.scenes[0].devices).has_length(3);
 
-        assert!(matches!(room.scenes[0].devices[0], Device::HueWhiteAndColorAmbiance{..}));
+        assert!(matches!(room.scenes[0].devices[0], Device::HueScene{..}));
         assert!(matches!(room.scenes[0].devices[1], Device::WemoOutlet{..}));
         assert!(matches!(room.scenes[0].devices[2], Device::NanoleafLightPanels{..}));
     }
